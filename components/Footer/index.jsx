eb83b0392ec6b3a797429dfbedb5a42107a3eb3e
lulastray <luciaastray@gmail.com>
2020-10-16 11:26:20 +0200

    Add styles to header and footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -9,43 +9,47 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link href="/">
-        <a>
-          <Logo className={styles.logo} />
-        </a>
-      </Link>
-      <nav>
-        <ul className={styles.navList}>
-          <li>
-            <Link href="/">
-              <a className={styles.link}>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/portfolio">
-              <a className={styles.link}>Portfolio</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact">
-              <a className={styles.link}>Contáctame</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li>
-            <Behance />
-          </li>
-          <li>
-            <Twitter />
-          </li>
-          <li>
-            <Linkedin />
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.flexContainer}>
+        <div className={styles.footerFlex}>
+          <Link href="/">
+            <a>
+              <Logo className={styles.logo} />
+            </a>
+          </Link>
+          <nav className={styles.nav}>
+            <ul className={styles.navList}>
+              <li>
+                <Link href="/">
+                  <a className={styles.link}>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/portfolio">
+                  <a className={styles.link}>Portfolio</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact">
+                  <a className={styles.link}>Contáctame</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <nav>
+          <ul className={styles.socialLogos}>
+            <li>
+              <Behance />
+            </li>
+            <li>
+              <Twitter />
+            </li>
+            <li>
+              <Linkedin />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 };
